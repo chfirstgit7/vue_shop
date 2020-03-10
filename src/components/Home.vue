50fe1f525faa9f952aa65c4cfd3f51c15df5f601
chfirstgit7 <woxinxyy@163.com>
2020-03-10 13:36:10 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button type="info" @click="loginout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginout() {
+      // 清除存储的数据token
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
+
+
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+
 Vue.config.productionTip = false
 
 new Vue({
